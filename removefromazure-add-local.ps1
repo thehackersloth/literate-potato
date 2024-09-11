afb5b1ea42020e6b34b27e301cf1537ef4d92f05
Andrew Trainum <125774407+thehackersloth@users.noreply.github.com>
2024-09-11 10:23:44 -0500

    Update removefromazure-add-local.ps1

--- removefromazure-add-local.ps1
+++ removefromazure-add-local.ps1
@@ -10,6 +10,18 @@ New-LocalUser -Name $username -Password $password
 # Convert the password to a usable format
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password 
 
+# Prompt for the new PIN
+$pin = Read-Host -AsSecureString -Prompt "Enter the new PIN for the local account:"
+
+# Set the PIN for the newly created user
+Add-LocalUser -Name $username -PIN $pin
+
+# Configure the user to log in with a PIN
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device"
+$name = "DevicePasswordLessBuildVersion"
+$value = 2
+New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+
 # Remove the Azure AD work or school account from the device
 Remove-AzureADDeviceRegisteredUser -Credential $credential -Confirm:$false
 
